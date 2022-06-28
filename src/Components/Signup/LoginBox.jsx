b7634c6b3d3d8c3cec3018c73524d0367e533a1b
HARSH JAIN <71275600+harshcoder690@users.noreply.github.com>
2022-06-28 17:25:28 +0530

    changes

--- src/Components/Signup/LoginBox.jsx
+++ src/Components/Signup/LoginBox.jsx
@@ -31,23 +31,22 @@ export const LoginBox = () => {
 
   return (
     <div className="main_con">
-      <div className="h-full py-16 px-4">
-        <div className="bg-grey-lighter min-h-screen flex flex-col con1">
-          <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 con2">
-            <h3 className="text-3xl font-extrabold leading-6 my-5 text-black head-text">
+      <div>
+        <div className="con1">
+          <div className="con2">
+            <h3 className="head-text">
             BookLib
             </h3>
-            <div className="space-x-2">
-              <h1 className="mb-8 text-3xl text-center head">Log in</h1>
+            <div>
+              <h1 className="head">Log in</h1>
             </div>
-            <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-md con3">
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border-2 border-gray-300 con4">
+            <div className="con3">
+              <div className="con4">
                 {error && <Alert variant="danger">{error}</Alert>}
-                <form className="space-y-6" onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                   <div className="field">
                     <label
                       for="mail"
-                      className="block text-sm font-medium text-gray-700"
                     >
                       Email
                     </label>
@@ -61,7 +60,6 @@ export const LoginBox = () => {
                   <div className="field">
                     <label
                       for="password"
-                      className="block text-sm font-medium text-gray-700"
                     >
                       Password
                     </label>
--- src/Components/Signup/Signupbox.jsx
+++ src/Components/Signup/Signupbox.jsx
@@ -37,23 +37,22 @@ export const Signupbox = () => {
 
   return (
     <div className="main_con">
-      <div className="h-full py-16 px-4">
-        <div className="bg-grey-lighter min-h-screen flex flex-col con1">
-          <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 con2">
-            <h3 className="text-2xl font-extrabold leading-6 my-5 text-black head-text">
+      <div>
+        <div className="con1">
+          <div className="con2">
+            <h3 className="head-text">
             BookLib
             </h3>
-            <div className="space-x-2">
-              <h1 className="mb-8 text-3xl text-center head">Sign up</h1>
+            <div>
+              <h1 className="head">Sign up</h1>
             </div>
-            <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-md con3">
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border-2 border-gray-300 con4">
+            <div className="con3">
+              <div className="con4">
                 {error && <Alert variant="danger">{error}</Alert>}
-                <form className="space-y-6" onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                   <div className="field">
                     <label
                       for="mail"
-                      className="block text-sm font-medium text-gray-700"
                     >
                       Email
                     </label>
@@ -67,7 +66,6 @@ export const Signupbox = () => {
                   <div className="field">
                     <label
                       for="password"
-                      className="block text-sm font-medium text-gray-700"
                     >
                       Password
                     </label>
@@ -82,7 +80,6 @@ export const Signupbox = () => {
                   <div className="field">
                     <label
                       for="password"
-                      className="block text-sm font-medium text-gray-700"
                     >
                       Password Confirmation
                     </label>
